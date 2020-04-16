b95b1d316dfcec37077bd796783eb8813da36c60
Andrés Lou <lourios.andres@gmail.com>
2020-04-16 11:02:24 -0400

    added evaluation script for sentence clf

--- train_model_sentece.py
+++ train_model_sentece.py
@@ -1,11 +1,13 @@
 """
-Training script for the binary sentence classifier
+Training and evaluation script for the binary sentence classifier
 """
 import torch
 import gensim
+import numpy as np
 from src.models import SentenceClassifier
 from src.dataset import SentenceDataset
-from src.train import trainIters
+from src.train import trainIters, test
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -13,7 +15,7 @@ embeddings_file = "../frWac_non_lem_no_postag_no_phrase_200_cbow_cut100.bin"
 embeddings = gensim.models.KeyedVectors.load_word2vec_format(embeddings_file, binary=True, unicode_errors='ignore')
 embeddings_tensor = torch.FloatTensor(embeddings.vectors)
 pickle_file = "data/dataset_sentences_facts_non_facts20200311.pickle"
-dataset = SentenceDataset(pickle_file=pickle_file, embeddings_file=embeddings_file, n_sentences=32)
+dataset = SentenceDataset(pickle_file=pickle_file, embeddings_file=embeddings_file, n_sentences=16)
 tr = int(len(dataset)*.70)
 vd = int(len(dataset)*.10)
 ts = len(dataset) - tr - vd
@@ -22,7 +24,7 @@ model = SentenceClassifier(embeddings_tensor=embeddings_tensor).to(device)
 
 ## CONFIG
 batch_size = 8
-n_epochs = 10
+n_epochs = 1
 learning_rate = 1e-4
 weight_decay = 0
 clip = .2
@@ -40,3 +42,16 @@ t, v = trainIters(model,
     collate_fn="sentence")
 
 torch.save(model.state_dict(), "sentence_clf.pt")
+model.eval()
+pred = []
+targ = []
+for sentence, annotation in test_ds:
+    length = torch.tensor([len(sentence)]).to(device)
+    pred.append(model(sentence.unsqueeze(0), length))
+    targ.append(annotation)
+pred = torch.tensor(pred).unsqueeze(0)
+targ = torch.tensor(targ).unsqueeze(0)
+acc, prec, rec = test(targ, pred)
+with open("sentence_clf.eval", "w") as file:
+    file.write("accuracy\tprecision\trecall\n")
+    file.write("{:.4f}\t\t{:.4f}\t\t{:.4f}".format(acc, prec, rec))
